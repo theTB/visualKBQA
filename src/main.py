b8d442ca559b4a9597605fa20b09a597159abbfd
theTB <trapitbansal@gmail.com>
2016-12-17 16:22:22 -0500

    first commit, code layout

---
+++ src/main.py
@@ -0,0 +1,118 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+import argparse
+import os
+import sys
+from itertools import izip
+import time
+
+import image_networks
+import question_networks
+import probability_networks
+
+tf.set_random_seed(42)
+np.random.seed(42)
+# Why 42?: https://goo.gl/15uxva
+
+def input_placeholders(batchsize, maxqlen, maxalen, numans):
+    '''
+    Placeholders for
+        input questions, question mask
+        input answers, answer masks
+        correct answers
+    '''
+    ques_placeholder = tf.placeholder(tf.int32, shape=(batchsize, maxqlen))
+    ques_mask_placeholder = tf.placeholder(
+        tf.int32, shape=(batchsize, maxqlen)
+    )
+    ans_placeholder = tf.placeholder(
+        tf.int32, shape=(batchsize, numans, maxalen)
+    )
+    ans_mask_placeholder = tf.placeholder(
+        tf.int32, shape=(batchsize, numans, maxalen)
+    )
+    label_placeholder = tf.placeholder(
+        tf.int32, shape=(batchsize, 1)
+    )
+
+    return ques_placeholder, ques_mask_placeholder,
+           ans_placeholder, ans_mask_placeholder,
+           label_placeholder
+
+
+def get_optimizer(config, step):
+    if config.optimizer == 'sgd':
+        learning_rate = tf.train.exponential_decay(
+            config.lr, step, config.decay_steps,
+            config.decay_rate, staricase=True
+        )
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    elif config.optimizer == 'adam':
+        # not sure if dcaying learning_rate is a good idea here
+        optimizer = tf.train.AdamOptimizer(learning_rate=config.lr)
+    elif config.optimizer == 'adagrad':
+        optimizer = tf.train.AdagradOptimizer(config.lr, step)
+    elif config.optimizer == 'adadelta':
+        optimizer = tf.train.AdadeltaOptimizer(learning_rate=config.lr)
+    elif config.optimizer == 'momentum':
+        optimizer = tf.train.MomentumOptimizer(config.lr, config.momentum)
+    else:
+        print ("Incorrect Optimizer specified")
+        sys.exit()
+
+    return optimizer
+
+
+def main(config):
+
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(description='Matrix tree parser')
+
+    p.add_argument('--data-file', required=True, type=str,
+                   help='path to parsing dataset')
+    p.add_argument('--log-dir', required=True, type=str,
+                   help='path to log directory')
+    p.add_argument('--val-ratio', default=0.05, type=float,
+                   help='ratio for validation split')
+    p.add_argument('--maxqlen', default=-1, type=int, help='max question length')
+    p.add_argument('--maxalen', default=-1, type=int, help='max answer length')
+    p.add_argument('--batchsize', default=128, type=int, help='batchsize')
+    p.add_argument('--emb-dim', default=100, type=int,
+                   help='size of word embeddings')
+    p.add_argument('--emb-file', default=None, type=str,
+                   help='initialization file for word embeddings')
+    p.add_argument('--optimizer', default='adam', type=str,
+                   help=
+                   'type of optimizer: sgd, adam, adadelta, adagrad, momentum')
+    p.add_argument('--lr', default=0.01, type=float, help='learing rate')
+    p.add_argument('--momentum', default=0.9,
+                   type=float, help='momentum, only for sgd')
+    p.add_argument('--decay-steps', default=1000, type=int,
+                   help='steps for lr decay')
+    p.add_argument('--decay-rate', default=0.96, type=float,
+                   help='rate for lr decay')
+    p.add_argument('--epochs', default=3, type=int,
+                   help='number of train epochs')
+    p.add_argument('--val-freq', default=20, type=int,
+                   help='validation frequency')
+    p.add_argument('--grad-clip', default=10.0, type=float,
+                   help='clip l2 norm of gradients at this value')
+    p.add_argument('--nhidden', default=100, type=int,
+                   help='number of final hidden units, only for some models')
+    p.add_argument('--nlayers', default=1, type=int,
+                   help='number of layers if using LSTM model')
+    p.add_argument('--dropout1', default=1, type=float,
+                   help='input dropout')
+    p.add_argument('--dropout2', default=1, type=float,
+                   help='dropout after layer 1')
+
+    config = p.parse_args()
+
+    main(config)
---
+++ src/text_representations.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+
+def embedding_lookup(text, mask, V, K, initialize=None):
+    '''
+    Given the text, create a embedding lookup
+    Args:
+        text: (tf.placeholder) the input tokens sequence
+        mask: (tf.placeholder) the input sequence mask
+        V: size of vocabulary
+        K: size of embeddings
+        initialize: (string) path to embeddings for initialization
+    Returns:
+        embed: (Tensor) B x maxlen x K tensor of embeddings
+    '''
+    pad_embedding = tf.constant(
+        np.zeros((1, K), dtype=np.float64), name="pad_embedding"
+    )
+    embeddings = tf.concat(0,
+        [pad_embedding,
+         tf.Variable(tf.random_uniform([V, K], -.001, .001, dtype=tf.float64),
+         name='embeddings')
+        ]
+    )
+    embed = tf.nn.embedding_lookup(embeddings, text)
+
+    return embed
+
+
+def bidir_lstm_model(
+        text, mask, V, K, nhidden, nlayers=1
+        peepholes=False, initialize=None, dropouts=None
+    ):
+    '''
+        Args:
+            text: (batchsize X maxlen) input text
+            mask: (batchsize X maxlen) input mask
+            V: vocabulary size
+            K: word-embedding dimension
+            nhiddne: number of hidden units of LSTM
+            nlayers: number of layers for the LSTM
+            peepholes: whether to use peephole connections
+            initialize: initialization file for word embeddings
+            dropouts: list of placeholders for the dropouts (size = nlayers+1)
+        Returns:
+            LSTM embeddings (batchsize X maxlen x hiddens)
+    '''
+    embeddings = embedding_lookup(text, mask, V, K, initialize)
+
+    if dropouts:
+        inputs = tf.nn.dropout(embeddings, dropouts[0])
+    else:
+        inputs = embeddings
+
+    for layer in xrange(nlayers):
+        fwd_cell = tf.nn.rnn_cell.LSTMCell(
+            nhidden, use_peepholes=peepholes, state_is_tuple=True
+        )
+        bwd_cell = tf.nn.rnn_cell.LSTMCell(
+            nhidden, use_peepholes=peepholes, state_is_tuple=True
+        )
+        lstm_outputs, _ = tf.nn.bidirectional_dynamic_rnn(
+            cell_fw=fwd_cell, cell_bw=bwd_cell, inputs=embeddings,
+            sequence_length=tf.cast(tf.reduce_sum(mask, 1), tf.int32),
+            dtype=tf.float64
+        )
+        fwd_outputs, bwd_outputs = lstm_outputs
+        inputs = tf.concat(2, [fwd_outputs, bwd_outputs])
+        if dropouts:
+            inputs = tf.nn.dropout(inputs, dropouts[layer+1])
+        if layer < nlayers-1: nhidden = int(nhidden / 2)
+
+
+    return inputs
