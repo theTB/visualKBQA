ac1bb1448a8fbe08aff521a4d52afc03fc900564
Huaizu Jiang <hzjiang@Huaizus-MacBook-Pro.local>
2016-12-18 13:39:59 -0500

    read data

--- src/image_networks.py
+++ src/image_networks.py
@@ -4,3 +4,24 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+
+
+def image_cnn():
+	'''
+	BUild a convolutional neural network (CNN) to get an embedding of an image.
+	'''
+	pass
+
+def get_image_embedding():
+	pass
+
+def load_image_embedding(data_dir, image_id):
+	'''
+	Load 4096-dimensional image embedding using a VGG16 CNN pre-trained on ImageNet.
+	'''
+	file_name = os.path.join(data_dir, image_name)
+	data = np.load(file_name)
+	im_embed = data['im_embed']
+	data.close()
+
+	return im_embed
---
+++ src/read_data.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import json
+
+def read_visual7w_dataset(qa_file, ground_annot_file):
+	pass
+
+def append_image_vqa_results(vqa_data, vqa):
+
+def vqa_data_iterator(vqa_data, batch_size):
+	pass
