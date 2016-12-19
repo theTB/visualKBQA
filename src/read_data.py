3aad8fe64f59b768c569b99f941bb61f4a011907
Huaizu Jiang <hzjiang@vonmises.cs.umass.edu>
2016-12-18 21:23:13 -0500

    tokenization

--- src/read_data.py
+++ src/read_data.py
@@ -4,7 +4,23 @@ from __future__ import print_function
 
 import numpy as np
 import json
+import cPickle
+import os
+import string
 
+# Special vocabulary symbols - we always put them at the start.
+_PAD = b"_PAD"
+_GO = b"_GO"
+_EOS = b"_EOS"
+_UNK = b"_UNK"
+_START_VOCAB = [_PAD, _GO, _EOS, _UNK]
+
+PAD_ID = 0
+GO_ID = 1
+EOS_ID = 2
+UNK_ID = 3
+
+# -------------------------------- dataset IO ------------------------------------------
 def read_visual7w_dataset(qa_file, ground_annot_file):
   '''
   Read question and multiple answers for each qa pair.
@@ -122,15 +138,122 @@ def append_image_embeddings(vqa_data, image_embeddings_file):
       im_embed      a numpy array of float (1x4096)
   '''
   with open(image_embeddings_file, 'r') as f:
-    image_id_embed = json.load(f)
+    image_id_embed = cPickle.load(f)
 
   for split in ['train', 'val', 'test']:
     for idx in xrange(len(vqa_data[split])):
       image_id = vqa_data[split][idx]['image_id']
       vqa_data[split][idx]['im_embed'] = image_id_embed[image_id]
 
-def preprocess_raw_data(vqa_data):
-  pass
-  
+  return vqa_data
+
+# -------------------------------- data preprocessing ------------------------------------------
+def tokenize_sentence(sentence): 
+  """
+  Tokenize a sentence.
+  Args:
+    sentence: a sentence
+  Returns:
+    tokens: tokenized sentence, a list
+  """ 
+  tokens = str(sentence).lower().translate(None, string.punctuation).strip().split()
+  return tokens
+
+def build_vocabulary(vqa_data, word_count_threshold, verbose=False):
+  """
+  Build a vocabulary.
+  Args:
+    data: comes from _read_data_file
+    word_count_threshold: words which occur less than word_count_threshold times would be converted to specil UNK tokens
+  """
+  count_thr = word_count_threshold
+
+  # count up the number of words
+  counts = {}
+  for split in ['train', 'val']:        # test set is not allowed to build the vocabulary
+    for qd in vqa_data[split]:
+        for w in tokenize_sentence(qd['question']):
+          counts[w] = counts.get(w, 0) + 1
+
+        for mc in qd['multiple_choices']:
+          for w in tokenize_sentence(mc):
+            counts[w] = counts.get(w, 0) + 1
+
+  # vocabulary, keep words that occur often
+  vocab_list = [w for w,n in counts.iteritems() if n > count_thr]
+  vocab_list = _START_VOCAB + vocab_list
+  vocab = dict([(x, y) for (y, x) in enumerate(vocab_list)])
+
+  # print some stats
+  if verbose:
+    cw = sorted([(count,w) for w,count in counts.iteritems()], reverse=True)
+    print 'top words and their counts:'
+    print '\n'.join(map(str,cw[:20]))
+    print 'most infrequent words and their counts:'
+    print '\n'.join(map(str,cw[-20:]))
+
+    total_words = sum(counts.itervalues())
+    print 'total words:', total_words
+    bad_words = [w for w,n in counts.iteritems() if n <= count_thr]
+    word_freq = [n for w, n in counts.iteritems() if n > count_thr]
+    bad_count = sum(counts[w] for w in bad_words)
+    print 'number of bad words: %d/%d = %.2f%%' % (len(bad_words), len(counts), len(bad_words)*100.0/len(counts))
+    print 'number of words in vocab would be %d' % (len(vocab), )
+    print 'number of UNKs: %d/%d = %.2f%%' % (bad_count, total_words, bad_count*100.0/total_words)
+
+    # lets look at the distribution of question lengths
+    q_lengths = {}
+    for split in ['train', 'val']:        # test set is not allowed to build the vocabulary
+      for qd in vqa_data[split]:
+        nw = len(tokenize_sentence(qd['question']))
+        q_lengths[nw] = q_lengths.get(nw, 0) + 1
+    max_q_len = max(q_lengths.keys())
+    print 'max length question in the train+val data: ', max_q_len
+    print 'question length distribution (count, number of words):'
+    sum_q_len = sum(q_lengths.values())
+    for i in xrange(max_q_len + 1):
+      print '%2d: %10d   %f%%' % (i, q_lengths.get(i,0), q_lengths.get(i,0)*100.0/sum_len)
+
+    # let's look at the distribution of answers lengths as well
+    a_lengths = {}
+    for split in ['train', 'val']:
+      for qd in vqa_data[split]:
+        for mc in qd['multiple_choices']:
+          nw = len(tokenize_sentence(mc))
+          a_lengths[nw] = a_lengths.get(nw, 0) + 1
+    max_a_len = max(a_lengths.keys())
+    print 'max length answer in the train+val data: ', max_a_len
+    print 'answer length distribution (count, number of words):'
+    sum_a_len = sum(a_lengths.values())
+    for i in xrange(max_a_len + 1):
+      print '%2d: %10d   %f%%' % (i, a_lengths.get(i,0), a_lengths.get(i,0)*100.0/sum_len)
+
+  return vocab
+
+def encode_question_answer(vqa_data, vocabulary):
+  """Tokenize data and turn into token-ids using given vocabulary.
+  Args:
+    data: a list of dict object.
+    vocabulary: pre-built vocabulary, a dict
+  Returns:
+    data: a list of dict objects, where token ids are stored in data[i]['token_ids']
+  """
+  for split in ['train', 'val', 'test']:
+    for i, qd in enumerate(vqa_data[split]):
+      words = tokenize_sentence(qd['question'])
+      vqa_data[split][i]['question_tk_ids'] = [vocabulary.get(w, UNK_ID) for w in words]
+
+      for j, mc in enumerate(qd['multiple_choices']):
+        words = tokenize_sentence(mc)
+        vqa_data[split][i]['multiple_choices'][j] = [vocabulary.get(w, UNK_ID) for w in words]
+
+  return vqa_data
+
+def preprocess_raw_data(vqa_data, word_count_threshold, verbose=False):
+  vocab = build_vocabulary(vqa_data, word_count_threshold, verbose)
+  vqa_data = encode_question_answer(vqa_data, vocab)
+
+  return vqa_data, vocab
+
 def vqa_data_iterator(vqa_data, batch_size):
 	pass
